da991dd0057ad09dcc46c2aec36fa4569b79d099
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-05-26 22:34:23 -0300

    Object from entries, map

--- ObjectEntries.js
+++ ObjectEntries.js
@@ -9,3 +9,11 @@ const entries = Object.entries(obj)
 for (const [key, value] of entries) {
 	console.log(`the value of ${key} is ${value}.`)
 }
+
+const map = new Map(entries)
+
+console.log({ map })
+
+const obj2 = Object.fromEntries(map)
+
+console.log({ obj2 }
