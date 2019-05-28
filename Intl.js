e2ab134232e4da666050260d7781152e6c3816ab
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-05-28 00:43:40 -0300

    Examples for MDN docs translation
    https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RelativeTimeFormat

--- Intl.js
+++ Intl.js
@@ -5,3 +5,32 @@ const rtf = new Intl.RelativeTimeFormat(lang, {numeric: 'auto'})
 const test = rtf.format(-1, 'year')
 
 console.log({test})
+
+let lan = 'pt'
+
+const rtf1 = new Intl.RelativeTimeFormat(lan, {numeric: 'always', style: 'long'})
+
+const test2 = rtf1.format(1, 'day')
+
+console.log({test2})
+
+console.log(Intl.RelativeTimeFormat.supportedLocalesOf(['pt', 'en', 'es', 'asda']))
+
+
+// Crie um formatador de tempo relativo no seu local
+// com os valores padrão sendo passados explicitamente.
+const rtf2 = new Intl.RelativeTimeFormat("pt", {
+    localeMatcher: "best fit", // outros valores: "lookup"
+    numeric: "auto", // outros valores: "auto"
+    style: "long", // outros valores: "short" ou "narrow"
+});
+
+// Formatação do tempo relativo usando valor negativo (-1).
+console.log(rtf2.format(-1, "day"));
+// > "há 1 dia"
+
+// Formatação do tempo relativo usando valor positivo (1).
+console.log(rtf2.format(1, "day"));
+// > "em 1 dia"
+
+console.log(rtf.formatToParts(100, "day"));
