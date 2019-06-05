86b668ab844c839428e87b5f2990caa9655bc3b6
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-06-04 22:18:11 -0300

    arrayFromObjectValues
    Useful for creating xlsx lines from object

--- ObjectEntries.js
+++ ObjectEntries.js
@@ -17,3 +17,22 @@ console.log({ map })
 const obj2 = Object.fromEntries(map)
 
 console.log({ obj2 }
+	    
+	    
+const obj3 = {
+	name: 'joao',
+    job: 'dev',
+    age: 21,
+    wharev: 'test',
+    nested: {
+    	test: 'test'
+    }
+}
+
+function arrayFromObjectValues(obj) {
+	return Object.entries(obj).map(x => x[1])
+}
+
+const arr3 = arrayFromObjectValues(obj3) 
+
+console.log(arr)
