43c49193bd1624dfc7e75d659ff049a48a7ff13c
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-06-26 22:52:09 -0300

    Update

--- Functions/Strings/searchCaseInsensitive.ts
+++ Functions/Strings/searchCaseInsensitive.ts
@@ -1,59 +1,50 @@
-export const searchCaseInsensitive = (...params: Array < any > ): (search:
-    any) => boolean => {
-        let validSearch
+export const searchCaseInsensitive = (...params: Array<any>): (search: any, partialMatch?: boolean) => boolean => {
+  let validSearch
 
-        // suport for older platforms without array.flat()
-        const flatArr = arr => arr.reduce((x, y) => x.concat(y), [])
-        const format = value => String(value).toUpperCase()
-        
-        if (Array.isArray(params)) {
-            validSearch = flatArr(params).map(param => {
-                return Array.isArray(param) ? param.map(format) :
-                    format(param)
-            })
-        } else {
-            validSearch = format(params)
-        }
+  // suport for older platforms without array.flat()
+  const flattenArr = arr => arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArr(val)) : acc.concat(val), [])
+  const format = value => String(value).toUpperCase()
 
-        return (search) => {
+  if (Array.isArray(params)) {
+      validSearch = flattenArr(params).map(param => {
+          return Array.isArray(param) ? param.map(format) : format(param)
+      })
+  } else {
+      validSearch = format(params)
+  }
 
-            const validation = valid => valid.includes(format(search))
+  return (search, partialMatch = true) => {
+    
+    
+      const validation = valid => valid.includes(format(search))
 
-            return Array.isArray(validSearch) ? validSearch.some(
-                validation) : validation(validSearch)
-        }
-    }
+      if(Array.isArray(validSearch)){
+        return partialMatch ? validSearch.some(validation) : validation(validSearch)
+      } else {
+        return partialMatch ? validation(validSearch) : validSearch === search 
+      }
+         
+  }
+}
 
-const list = [{
-        name: 'john',
-        skills: ['js', 'node', 'react', 'react-native']
-    },
-    {
-        name: 'pete',
-        skills: ['ruby', 'rust', 'rails', 'angular']
-    },
-    {
-        name: 'dave',
-        skills: ['python', 'django', 'sql', 'mongo']
-    },
-    {
-        name: 'matt',
-        skills: ['java', 'android', 'jsf', 'kotlin']
-    },
-    {
-        name: 'jeff',
-        skills: ['ios', 'swift', 'objective-c', 'xcode']
-    },
-    {
-        name: 'kyle',
-        skills: ['c', 'assembly', 'c#', 'f#']
-    },
+const list = [
+  {name: 'john', skills: ['js', 'node', 'react', 'react-native']},
+  {name: 'pete', skills: ['ruby', 'rust', 'rails', 'angular']},
+  {name: 'dave', skills: ['python', 'django', 'sql', 'mongo']},
+  {name: 'matt', skills: ['java', 'android', 'jsf', 'kotlin']},
+  {name: 'jeff', skills: ['ios', 'swift', 'objective-c', 'xcode']},
+  {name: 'kyle', skills: ['c', 'assembly', 'c#', 'f#']},
 ]
 
-const params = list.map(({
-    skills
-}) => skills)
+const skills = list.map(({skills})=> skills)
 
-const isValid = searchCaseInsensitive(params)('django')
+const names = list.map(({name})=> name)
+//console.log(params)
 
-//isValid
+const isValid = searchCaseInsensitive(skills, names)('djan', false)
+
+// isValid false
+
+const isValidMatchAll = searchCaseInsensitive(skills, names)('djan', true)
+
+// isValidMatchAll true
