1cbe71f424faffeca428d87321241f562090b368
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-06-29 14:15:11 -0300

    Use object.values instead

--- Functions/Objects/arrayFromObjectValues.js
+++ Functions/Objects/arrayFromObjectValues.js
@@ -12,3 +12,17 @@ const arr = arrayFromObjectValues(obj)
 console.log(arr)
 
 // ["john", "21"]
+
+// actually useless:
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+
+const obj2 = {
+	name: 'john',
+    age: '21'
+}
+
+const arr2 = Object.values(obj2)
+
+console.log({arr2})
+
+// ["john", "21"]
