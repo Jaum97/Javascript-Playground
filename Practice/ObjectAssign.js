94a81233db72a7c5a947602211fffc79cc6a88f4
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-09-17 22:45:51 -0300

    create

---
+++ Practice/ObjectAssign.js
@@ -0,0 +1,13 @@
+var o1 = { a: 1 };
+var o2 = { b: 2 };
+var o3 = { c: 3 };
+
+var obj = Object.assign(o1, o2, o3);
+console.log(obj); // { a: 1, b: 2, c: 3 }
+console.log(o1);  // { a: 1, b: 2, c: 3 }, target object itself is changed.
+
+var o1 = { a: 1 };
+var o2 = { [Symbol('foo')]: 2 };
+
+var obj = Object.assign({}, o1, o2);
+console.log(obj); // { a: 1, [Symbol("foo")]: 2 }
