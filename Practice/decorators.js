6671c640436a6d8e75e4f9339244b698825f3144
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-12-19 00:48:45 -0300

    create
    very cool!

---
+++ Practice/decorators.js
@@ -0,0 +1,31 @@
+//https://www.sitepoint.com/javascript-decorators-what-they-are/
+
+function log(target, name, descriptor) {
+  const original = descriptor.value;
+  if (typeof original === 'function') {
+    descriptor.value = function(...args) {
+      console.log(`Arguments: ${args}`);
+      try {
+        const result = original.apply(this, args);
+        console.log(`Result: ${result}`);
+        return result;
+      } catch (e) {
+        console.log(`Error: ${e}`);
+        throw e;
+      }
+    }
+  }
+  return descriptor;
+}
+
+class Example {
+  @log
+  sum(a, b) {
+    return a + b;
+  }
+}
+
+const e = new Example();
+e.sum(1, 2);
+//Arguments: 1,2 ​​​​​at ​​​`Arguments: ${ args }`​​​ ​quokka.ts:5:6​
+//Result: 3 ​​​​​at ​​​`Result: ${ result }`​​​ ​quokka.ts:8:8​
